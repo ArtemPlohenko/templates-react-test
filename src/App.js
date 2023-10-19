52e4e410f80a70e3e0bb45b83928ba74e52d2093
artemplohenko@gmail.com <artemplohenko@gmail.com>
2023-10-19 14:39:15 +0300

    added templates

--- src/App.js
+++ src/App.js
@@ -1,23 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+import { Component } from "react";
+import "./App.css";
+
+// function WhoAmi({ name, surname, link }) {
+//   return (
+//     <div>
+//       <h1>
+//         {/* My name is {name.firstName}, surname - {surname} */}
+//         My name is {name()}, surname - {surname}
+//       </h1>
+//       <a href={link}>My profile</a>
+//     </div>
+//   );
+// }
+
+// class
+class WhoAmi extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      years: 27,
+    };
+  }
+
+  nextYear = () => {
+    console.log("+++");
+    // this.state.years++; // error
+    this.setState({
+      years: this.state.years + 1,
+    });
+  };
+
+  render() {
+    const { name, surname, link } = this.props;
+    return (
+      <div>
+        <button onClick={this.nextYear}>+++</button>
+        <h1>
+          {/* My name is {name.firstName}, surname - {surname} */}
+          {/* My name is {name()}, surname - {surname} */}
+          My name is {name}, surname - {surname}, age - {this.state.years}
+        </h1>
+        <a href={link}>My profile</a>
+      </div>
+    );
+  }
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <WhoAmi name={{ firstName: "John" }} surname="Smith" link="facebook.com" />
+      <WhoAmi name={{ firstName: "Alex" }} surname="Shepard" link="facebook.com" /> */}
+
+      {/* <WhoAmi
+        name={() => {
+          return "John";
+        }}
+        surname="Smith"
+        link="facebook.com"
+      />
+      <WhoAmi
+        name={() => {
+          return "Alex";
+        }}
+        surname="Shepard"
+        link="facebook.com"
+      /> */}
+
+      <WhoAmi name="John" surname="Smith" link="facebook.com" />
+      <WhoAmi name="Alex" surname="Shepard" link="facebook.com" />
     </div>
   );
 }
