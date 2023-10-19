5a8578caacc3939c79fdc07e789c2d982d6881a3
artemplohenko@gmail.com <artemplohenko@gmail.com>
2023-10-19 19:48:01 +0300

    context and argument

--- src/App.js
+++ src/App.js
@@ -7,8 +7,16 @@ class WhoAmi extends Component {
     this.state = {
       years: 27,
       text: "+++",
+      position: "",
     };
+    // this.nextYear = this.nextYear.bind(this);
   }
+
+  // nextYear() {
+  //   this.setState((state) => ({
+  //     years: state.years + 1,
+  //   }));
+  // }
 
   nextYear = () => {
     console.log("+++");
@@ -17,15 +25,32 @@ class WhoAmi extends Component {
     }));
   };
 
+  commitInputChanges = (e, color) => {
+    // console.log(e.target.value);
+    // console.log(color);
+    this.setState({
+      position: e.target.value,
+    });
+  };
+
   render() {
     const { name, surname, link } = this.props;
+    const { position, years } = this.state;
+
+    // console.log(this);
+
     return (
       <div>
         <button onClick={this.nextYear}>{this.state.text}</button>
         <h1>
-          My name is {name}, surname - {surname}, age - {this.state.years}
+          My name is {name}, surname - {surname}, age - {years}, position - {position}
         </h1>
         <a href={link}>My profile</a>
+
+        <form>
+          <span>Enter the position</span>
+          <input type="text" onChange={(e) => this.commitInputChanges(e, "some color")} />
+        </form>
       </div>
     );
   }
