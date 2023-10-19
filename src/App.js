6cf66a930e7a2962667d252ca0f0e7809539912a
artemplohenko@gmail.com <artemplohenko@gmail.com>
2023-10-19 14:50:33 +0300

    added state component

--- src/App.js
+++ src/App.js
@@ -1,43 +1,28 @@
 import { Component } from "react";
 import "./App.css";
 
-// function WhoAmi({ name, surname, link }) {
-//   return (
-//     <div>
-//       <h1>
-//         {/* My name is {name.firstName}, surname - {surname} */}
-//         My name is {name()}, surname - {surname}
-//       </h1>
-//       <a href={link}>My profile</a>
-//     </div>
-//   );
-// }
-
-// class
 class WhoAmi extends Component {
   constructor(props) {
     super(props);
     this.state = {
       years: 27,
+      text: "+++",
     };
   }
 
   nextYear = () => {
     console.log("+++");
-    // this.state.years++; // error
-    this.setState({
-      years: this.state.years + 1,
-    });
+    this.setState((state) => ({
+      years: state.years + 1,
+    }));
   };
 
   render() {
     const { name, surname, link } = this.props;
     return (
       <div>
-        <button onClick={this.nextYear}>+++</button>
+        <button onClick={this.nextYear}>{this.state.text}</button>
         <h1>
-          {/* My name is {name.firstName}, surname - {surname} */}
-          {/* My name is {name()}, surname - {surname} */}
           My name is {name}, surname - {surname}, age - {this.state.years}
         </h1>
         <a href={link}>My profile</a>
@@ -49,24 +34,6 @@ class WhoAmi extends Component {
 function App() {
   return (
     <div className="App">
-      {/* <WhoAmi name={{ firstName: "John" }} surname="Smith" link="facebook.com" />
-      <WhoAmi name={{ firstName: "Alex" }} surname="Shepard" link="facebook.com" /> */}
-
-      {/* <WhoAmi
-        name={() => {
-          return "John";
-        }}
-        surname="Smith"
-        link="facebook.com"
-      />
-      <WhoAmi
-        name={() => {
-          return "Alex";
-        }}
-        surname="Shepard"
-        link="facebook.com"
-      /> */}
-
       <WhoAmi name="John" surname="Smith" link="facebook.com" />
       <WhoAmi name="Alex" surname="Shepard" link="facebook.com" />
     </div>
