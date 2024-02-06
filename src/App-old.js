045721cb2b08e2c69ac6b7999c675d2def814a9c
artemplohenko@gmail.com <artemplohenko@gmail.com>
2024-02-06 20:55:40 +0200

    added ref

---
+++ src/App-old.js
@@ -0,0 +1,172 @@
+// import React, { Component } from "react";
+// import styled from "styled-components";
+// import BootstrapTest from "./BootstrapTest";
+
+// import "./App.css";
+
+// const EmpItem = styled.div`
+//   padding: 20px;
+//   margin-bottom: 15px;
+//   border-radius: 5px;
+//   box-shadow: 5px 5px 10px rgba(0, 0, 0, 0.2);
+//   a {
+//     display: block;
+//     margin: 10px 0;
+//     color: ${(props) => (props.active ? "orange" : "black")};
+//   }
+//   input {
+//     display: block;
+//     margin-top: 10px;
+//   }
+// `;
+
+// const Header = styled.h2`
+//   font-size: 22px;
+// `;
+
+// export const Button = styled.button`
+//   display: block;
+//   padding: 5px 15px;
+//   background-color: gold;
+//   border: 1px solid rgba(0, 0, 0, 0.2);
+//   box-shadow: 5px 5px 10px rgba(0, 0, 0, 0.2);
+//   &:hover {
+//     background-color: #ff6666;
+//   }
+// `;
+
+// class WhoAmi extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       years: 27,
+//       text: "+++",
+//       position: "",
+//     };
+//     // this.nextYear = this.nextYear.bind(this);
+//   }
+
+//   // nextYear() {
+//   //   this.setState((state) => ({
+//   //     years: state.years + 1,
+//   //   }));
+//   // }
+
+//   nextYear = () => {
+//     console.log("+++");
+//     this.setState((state) => ({
+//       years: state.years + 1,
+//     }));
+//   };
+
+//   commitInputChanges = (e, color) => {
+//     // console.log(e.target.value);
+//     // console.log(color);
+//     this.setState({
+//       position: e.target.value,
+//     });
+//   };
+
+//   render() {
+//     const { name, surname, link } = this.props;
+//     const { position, years } = this.state;
+
+//     // console.log(this);
+
+//     return (
+//       <EmpItem active>
+//         <Button onClick={this.nextYear}>{this.state.text}</Button>
+//         <Header>
+//           My name is {name}, surname - {surname}, age - {years}, position - {position}
+//         </Header>
+//         <a href={link}>My profile</a>
+
+//         <form>
+//           <span>Enter the position</span>
+//           <input type="text" onChange={(e) => this.commitInputChanges(e, "some color")} />
+//         </form>
+//       </EmpItem>
+//     );
+//   }
+// }
+
+// const Wrapper = styled.div`
+//   width: 600px;
+//   margin: 80px auto 0 auto;
+// `;
+
+// const DynamicGreating = (props) => {
+//   return (
+//     <div className={"mb-3 p-3 border border-" + props.color}>
+//       {/* {props.children} */}
+//       {React.Children.map(props.children, (child) => {
+//         return React.cloneElement(child, { className: "shadow p-3 m-3 border rounded" });
+//       })}
+//     </div>
+//   );
+// };
+
+// const HelloGreating = () => {
+//   return (
+//     <div style={{ width: "600px", margin: "0 auto" }}>
+//       <DynamicGreating color={"primary"}>
+//         <h2>Hello world!</h2>
+//       </DynamicGreating>
+//     </div>
+//   );
+// };
+
+// const Message = (props) => {
+//   return <h2>The counter is {props.counter}</h2>;
+// };
+
+// class Counter extends Component {
+//   state = {
+//     counter: 0,
+//   };
+
+//   changeCounter = () => {
+//     this.setState(({ counter }) => ({
+//       counter: counter + 1,
+//     }));
+//   };
+
+//   render() {
+//     return (
+//       <>
+//         <button className={"btn btn-primary"} onClick={this.changeCounter}>
+//           Click me
+//         </button>
+//         {this.props.render(this.state.counter)}
+//       </>
+//     );
+//   }
+// }
+
+// function App() {
+//   return (
+//     <Wrapper>
+//       <Counter render={(counter) => <Message counter={counter} />} />
+
+//       <HelloGreating />
+//       <BootstrapTest
+//         left={
+//           <DynamicGreating color={"primary"}>
+//             <h2>This weel was hars</h2>
+//             <h2>Hello world!</h2>
+//           </DynamicGreating>
+//         }
+//         right={
+//           <DynamicGreating color={"primary"}>
+//             <h2>Right</h2>
+//           </DynamicGreating>
+//         }
+//       />
+
+//       <WhoAmi name="John" surname="Smith" link="facebook.com" />
+//       <WhoAmi name="Alex" surname="Shepard" link="facebook.com" />
+//     </Wrapper>
+//   );
+// }
+
+// export default App;
