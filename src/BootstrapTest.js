9eca11748d5b50886a82f20b01d8e5f01049d6ab
artemplohenko@gmail.com <artemplohenko@gmail.com>
2024-02-06 10:06:37 +0200

    committed imports

--- src/BootstrapTest.js
+++ src/BootstrapTest.js
@@ -2,9 +2,9 @@ import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 
-import Form from "react-bootstrap/Form";
-import Button from "react-bootstrap/Button";
-import Carousel from "react-bootstrap/Carousel";
+// import Form from "react-bootstrap/Form";
+// import Button from "react-bootstrap/Button";
+// import Carousel from "react-bootstrap/Carousel";
 
 function BootstrapTest(props) {
   return (
