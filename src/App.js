276ba63622ff34d429247fb8f54d8cd4eb99fb3e
artemplohenko@gmail.com <artemplohenko@gmail.com>
2024-02-06 10:03:44 +0200

    added Specialization

--- src/App.js
+++ src/App.js
@@ -106,9 +106,20 @@ const DynamicGreating = (props) => {
   );
 };
 
+const HelloGreating = () => {
+  return (
+    <div style={{ width: "600px", margin: "0 auto" }}>
+      <DynamicGreating color={"primary"}>
+        <h2>Hello world!</h2>
+      </DynamicGreating>
+    </div>
+  );
+};
+
 function App() {
   return (
     <Wrapper>
+      <HelloGreating />
       <BootstrapTest
         left={
           <DynamicGreating color={"primary"}>
